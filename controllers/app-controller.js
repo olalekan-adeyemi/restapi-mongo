7780772460fdfedbafed89b80566b84092c862a3
olalekan-adeyemi <olalekan.adeyemi@gmail.com>
2021-06-26 15:50:38 +0100

    implemented logic for adding and retrieving products

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -12,21 +12,43 @@ const getBase = (req, res, next) => {
 }
 
 const getProducts = (req, res) => {
-	res.status('200').json({message: 'Handling get route for products'})
+
+	Product.find().then(product => {
+		res.status('200').json({message: 'Handling get route for products',product: product})
+	}).catch(err => { 
+		console.log(err)
+		res.status('400').json({product: 'no products found',message: 'Handling get route for products'})
+	})
+	
 }
 
 const postProduct = (req, res) => {
+
 	const { name, price } = req.body
-	//const product = {name: name, price: price}
 
-	const product = new Product({
-		_id: new mongoose.Types.ObjectId(),
-		name: name,
-		price: price
-	})
+	if(name === undefined || price == undefined) {
 
-	product.save().then(result => console.log(result)).catch( err => console.log(err))
-	res.status('201').json({success: true, message: 'new product added'})
+		res.setHeader('Content-Type', 'application/json')
+		res.status('400').json({status: false, message: 'nothing addeed'})
+		
+	}else {
+		console.log(name, price)
+		//const product = {name: name, price: price}
+
+		const product = new Product({
+			_id: new mongoose.Types.ObjectId(),
+			name: name,
+			price: price
+		})
+
+		product.save().then(result => console.log(result)).catch( err => console.log(err))
+
+		res.setHeader('Content-Type', 'application/json')
+		res.status('201').json({success: true, message: 'new product added', product: product})
+
+	}
+
+	
 }
 
 
