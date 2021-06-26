08f50fc033ffff2d7fe6b2c0c992d71fbff82f35
olalekan-adeyemi <olalekan.adeyemi@gmail.com>
2021-06-26 13:49:41 +0100

    updated the app.js file

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
 require('dotenv').config()
 
 
 //APP SERVER
 const app = express()
 const PORT = process.env.PORT || 4000
+
+
+
+
+//declare middlewares
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+//route paths
+const appRoute = require('./routes/base-route')
+
+//add routes
+app.use(appRoute)
+
 
 
 
